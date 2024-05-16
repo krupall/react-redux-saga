930645cc9a7d53f6bcc3dbd057c10a3be1e4b94f
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:13:11 +0100

    Update TeamSummery.js

--- src/components/TeamSummery.js
+++ src/components/TeamSummery.js
@@ -1,12 +1,34 @@
-function TeamSummary(){
+import { useSelector } from "react-redux";
 
-    return(<div className="teamSummary">
-        <h3>Team Purse Summary</h3>
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react';
 
-        
+function TeamSummary() {
+  const dispatch = useDispatch()
+  const teamList = useSelector((state)=>state.teamData);
+  useEffect(()=>{
+    dispatch(teamList())
+   },[])
+  return (
+    <div className="teamSummary">
+      <h3>Team Purse Summary</h3>
+      <br></br>
 
-
-    </div>)
+ 
+      
+        {/* <img className="card-img-top" src="..." alt="Card image cap"/> */}
+        {teamList.map(e =>(
+          <div className="card team-card m-2">
+        <div className="card-body">
+        <h5 className="card-title">{e}</h5>
+        <p className="card-text">Sample Text.</p>
+        <a href="#" className="btn btn-primary">
+          Players
+        </a>
+      </div></div>
+      ))}
+    </div>
+  );
 }
 
 export default TeamSummary;
