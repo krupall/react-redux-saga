da4d5a2486732ec0a53c2fe0b75524ee106ab849
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:14:56 +0100

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,10 @@
 import {combineReducers} from 'redux'
 import { cartData } from './reducer'
 import { productData } from './productReducer'
-import {  playerData } from './playerReducer'
+import {  playerData, teamData } from './playerReducer'
 export default combineReducers({
     // cartData,
     // productData,
-    playerData
+    playerData,
+    teamData
 })
