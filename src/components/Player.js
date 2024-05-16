0dfd03eddea902639fe225d3bb31acb193c4937f
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:12:52 +0100

    Update Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,18 +2,51 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useDispatch } from 'react-redux'
 import { playerlist } from '../redux/playerAction';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import TeamSummary from "./TeamSummery";
 
 function Player(){
     
-    const userID = useParams('id').id;const dispatch = useDispatch();
+    const userID = useParams('id').id;
+    const dispatch = useDispatch();
     let data = useSelector((state)=>state.playerData);
-    useEffect(() => {
-        dispatch(playerlist())
-    },[userID])
+    const [team, setTeam] = useState('');
+    const [initialValue, setInitialValue] = useState(1000);
+  
     const player = data.filter(e => e.id == userID);
+    function updateBid (cat) {
+       console.log(cat);
+       if(cat === 'Gold'){
+        setInitialValue(initialValue + 1000)
+       } else setInitialValue(initialValue + 500)
+    }
     console.log(player[0].id)
-    return (<div>{player[0]?.profile?.name}</div>)
+    // const getInitialValue = (cat) => {
+    //     if(player[0].category === 'Gold'){
+    //         setInitialValue(8000);
+    //     } 
+    //     if(player[0].category === 'Silver'){
+    //         setInitialValue(5000);
+    //     }  
+    //     return initialValue
+    // }
+   
+    return (
+    <div>
+         <div>{player[0]?.profile?.name}</div>
+         <div>{player[0]?.profile?.about}</div>
+         <div>{player[0]?.profile?.dob}</div>
+         <div>{player[0]?.profile?.address}</div>
+         <div>{player[0]?.category}</div>
+         <div><img src={player[0]?.picture}/></div>
+         <div>{player[0]?.profile?.roles}</div>
+         <div>{player[0]?.profile?.name}</div>
+         <input type="text" className="m-2" value={initialValue}/>
+         <button className="btn btn-outline-danger" onClick={() =>updateBid(player[0]?.category)}> + </button>
+        
+        <TeamSummary players={false} selectedteam={team} ></TeamSummary>
+    </div>
+   )
 }
 
 export default Player;
