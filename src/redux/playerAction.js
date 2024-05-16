69b5b2988ded8bf7a537ea8889413392cab44799
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:14:02 +0100

    Update playerAction.js

--- src/redux/playerAction.js
+++ src/redux/playerAction.js
@@ -1,8 +1,15 @@
-import { PLAYER_LIST } from "./constant"
+import { PLAYER_LIST, TEAM_LIST } from "./constant"
 
 export const playerlist = () => {
   
     return {
         type: PLAYER_LIST,
+    }
+}
+
+export const teamlist = () => {
+  
+    return {
+        type: TEAM_LIST,
     }
 }
