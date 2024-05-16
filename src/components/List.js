b0a31c30e5cf3e621d67c1f898dd94eb79444257
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:10:50 +0100

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -2,22 +2,33 @@
 import {useSelector} from 'react-redux'
 
 import { useDispatch } from 'react-redux'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { playerlist } from '../redux/playerAction';
 import { Link } from 'react-router-dom';
 
 
 function List() {
     const dispatch = useDispatch();
-    let data = useSelector((state)=>state.playerData);
+    const playerData = useSelector((state)=>state.playerData)
+    const [listData, setlistData] = useState(playerData);
     
-    console.log(data);
-
+    
+    console.log(listData);
+  const callClick= (event) => {
+    if(event.currentTarget.checked === true){
+      setlistData(listData.filter(e => e.category === 'Gold'))
+    } else {
+      setlistData(playerData)
+    }
+  }
    
-      return ( <div className='square'>{
-        data.map((element, index) => {
+      return ( <div>
+         <input type='checkbox' onClick={(e) =>callClick(e)}  /> Golden Players
+        <div className='square'>{
+       
+       listData.map((element, index) => {
             let url = `/player/`+element.id;
-            return  <Link to={url}>{index + 1}</Link>})}</div>  );
+            return  <Link to={url}>{index + 1}</Link>})}</div>  </div> );
 }
 
 export default List;
