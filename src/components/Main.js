dbf8ec906786cdd610fca52fb20fdaf1a9f59cc4
krupal vasani <kvasani59@gmail.com>
2024-05-16 15:12:34 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,30 @@ import { useDispatch } from 'react-redux'
 import { productList } from '../redux/productAction';
 import {useSelector} from 'react-redux'
 import { useEffect } from 'react';
-import { playerlist } from '../redux/playerAction';
+import { playerlist, teamlist } from '../redux/playerAction';
 
 function Main() {
   const dispatch = useDispatch();
-  let data = useSelector((state)=>state.playerData);
-  console.warn("data in main component", data);
+  let playerData = useSelector((state)=>state.playerData);
+  let teamData = useSelector((state)=>state.teamData);
+  console.warn("playerData in main component", playerData);
+  console.warn("teamData in main component", teamData);
   
   useEffect(()=>{
-    dispatch(playerlist())
+   dispatch(playerlist())
+   dispatch(teamlist())
   },[])
+  
   return (
     <div>
       <div>
       {/* <button onClick={() => dispatch(emptyCart())}>Empty Cart</button> */}
       </div>
+      <div>Total Player : {playerData.length}</div>
+      <div>Total Teams : {teamData?.length}</div>
       <div className='product-container'>
         {
-          data.map((item)=><div className='product-item'>
+          playerData.map((item)=><div className='product-item'>
             <img src={item.picture} alt="" />
             <div className='name'> {item.profile.name} </div>
             <div>Category : {item.category} </div>
