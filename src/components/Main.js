24d808f666a0230dfdd2187bcbbf0903db9e7660
krupal <kvasani59@gmail.com>
2024-05-21 10:27:22 +0100

    team fixing

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import { productList } from '../redux/productAction';
 import {useSelector} from 'react-redux'
 import { useEffect } from 'react';
 import { playerlist, teamlist } from '../redux/playerAction';
+import TeamSummary from './TeamSummery';
 
 function Main() {
   const dispatch = useDispatch();
@@ -11,6 +12,8 @@ function Main() {
   let teamData = useSelector((state)=>state.teamData);
   console.warn("playerData in main component", playerData);
   console.warn("teamData in main component", teamData);
+  const goldPlayers = playerData?.filter(e => e.category === 'Gold');
+  const silverPlayers = playerData?.filter(e => e.category === 'Silver');
   
   useEffect(()=>{
    dispatch(playerlist())
@@ -22,8 +25,24 @@ function Main() {
       <div>
       {/* <button onClick={() => dispatch(emptyCart())}>Empty Cart</button> */}
       </div>
-      <div>Total Player : {playerData.length}</div>
+      <div className='header-detail'>
+        <h2>Auction Overview</h2>
+        <div className='detail'>
+      <div className='total'><span className='totalNumber'>{playerData.length}</span><p>Registrations</p></div>
+      <div className='category'>
+        GOLD PLAYER : {goldPlayers.length}
+        SOLD: {goldPlayers.filter(e => e.sold === true).length}
+        UNSOLD: {goldPlayers.filter(e => e.sold === false).length}
+      </div>
+      <div className='category'>
+        SILVER PLAYER : {silverPlayers.length}
+        SOLD: {silverPlayers.filter(e => e.sold === true).length}
+        UNSOLD: {silverPlayers.filter(e => e.sold === false).length}
+      </div>
+      </div>
+      </div>
       <div>Total Teams : {teamData?.length}</div>
+      <TeamSummary></TeamSummary>
       <div className='product-container'>
         {
           playerData.map((item)=><div className='product-item'>
--- src/components/TeamSummery.js
+++ src/components/TeamSummery.js
@@ -3,21 +3,21 @@ import { useSelector } from "react-redux";
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react';
 
+import { teamlist } from '../redux/playerAction';
+
 function TeamSummary() {
   const dispatch = useDispatch()
-  const teamList = useSelector((state)=>state.teamData);
+  let teamList = useSelector((state)=>state.teamData);
   useEffect(()=>{
-    dispatch(teamList())
+    dispatch(teamlist())
    },[])
   return (
     <div className="teamSummary">
       <h3>Team Purse Summary</h3>
       <br></br>
 
- 
-      
         {/* <img className="card-img-top" src="..." alt="Card image cap"/> */}
-        {teamList.map(e =>(
+        {teamList.length && teamList.map(e =>(
           <div className="card team-card m-2">
         <div className="card-body">
         <h5 className="card-title">{e}</h5>
