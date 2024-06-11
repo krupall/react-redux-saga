f286a759cb81c21eec32227dbb0911d08790db55
krupal vasani <kvasani59@gmail.com>
2024-06-11 12:46:12 +0100

    Update team data (#7)
    * team list sold
    
    * home page update
    
    * model changes

--- src/components/List.js
+++ src/components/List.js
@@ -14,6 +14,8 @@ function List() {
     const category = useParams('category').category;
     const playerData = useSelector((state)=>state.playerData)
     const [checkGoldcheckbox, setCheckGoldcheckbox] = useState(false)
+    const [soldPlayer, setSoldPlayer] = useState(0);
+    const [unsoldPlayer, setunSoldPlayer] = useState(0);
     const [checkSilvercheckbox, setCheckSilvercheckbox] = useState(false)
     const [listData, setlistData] = useState(playerData);
     
@@ -27,24 +29,36 @@ function List() {
         setCheckSilvercheckbox(true)
       }
       setlistData(listData.filter(e => e.category === category))
+      countSoldPlayers(listData.filter(e => e.category === category))
     } else {
       setCheckGoldcheckbox(false)
       setCheckSilvercheckbox(false);
       setlistData(playerData)
+      countSoldPlayers(playerData)
     }
+  }
+
+  const countSoldPlayers = (data) => {
+    let counter = 0;
+    counter = data.filter(e => e.sold === true);
+    setSoldPlayer(counter.length);
+    setunSoldPlayer(data.length - counter.length);
   }
 
   useEffect(() => {
     if(category === 'gold'){
       setCheckGoldcheckbox(true);
       setlistData(listData.filter(e => e.category === 'Gold'))
+      countSoldPlayers(listData)
     } 
     if(category === 'silver'){
       setCheckSilvercheckbox(true);
       setlistData(listData.filter(e => e.category === 'Silver'))
+      countSoldPlayers(listData)
     }
     if(category === 'all'){
       setlistData(playerData)
+      countSoldPlayers(playerData)
       setCheckGoldcheckbox(false)
       setCheckSilvercheckbox(false);
     }
@@ -53,7 +67,11 @@ function List() {
       return ( <div>
         <div className='categoryCheckboxGroup'>
           <input type='checkbox' className='p-3' checked={checkGoldcheckbox} onClick={(e) =>callClick(e ,'Gold')}  /> Gold Players
-          <input type='checkbox'  className='m-2' checked={checkSilvercheckbox} onClick={(e) =>callClick(e, 'Silver')}  /> Silver Players</div>
+          <input type='checkbox'  className='m-2' checked={checkSilvercheckbox} onClick={(e) =>callClick(e, 'Silver')}  /> Silver Players
+          &nbsp;<span>Sold: {soldPlayer}</span>
+          &nbsp;<span>UnSold: {unsoldPlayer}</span>
+          </div>
+         
         <div className='square'>{
        
        listData.map((element, index) => {
